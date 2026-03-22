81fa7585b7f6242b92b152f538b3a85083db0170
Gary Allen <gvcallen@gmail.com>
2026-03-22 16:09:39 +0200

    Removed parameter transform

--- parax/core/__init__.py
+++ parax/core/__init__.py
@@ -5,16 +5,14 @@ from parax.core.parameter import Parameter, is_param, is_valid_param, is_free_pa
 from parax.core.parameter_metadata import ParameterMetadata
 from parax.core.parameter_group import ParameterGroup
 from parax.core.tree import partition
-from parax.core.transform import ParameterTransform
 
 __all__ = [
-    "field",
-    "load",
-    "save",
-    "Module",
     "Parameter", "is_param", "is_valid_param", "is_free_param", "is_fixed_param", "as_param",
     "ParameterMetadata",
     "ParameterGroup",
     "partition",
-    "ParameterTransform",
+    "field",
+    "load",
+    "save",
+    "Module",
 ]

2d8a4af15aeca7fbbe0a7d18c0f11f888f22a7fc
Gary Allen <gvcallen@gmail.com>
2026-05-04 17:57:50 +0200

    Updated import structure

--- parax/__init__.py
+++ parax/__init__.py
@@ -18,7 +18,6 @@ from parax.tagged import (
 from parax.bounded import (
     AbstractBounded as AbstractBounded,
 )
-import parax.bounded as bounded
 
 from parax.unwrappables import (
     AbstractUnwrappable as AbstractUnwrappable,
@@ -76,4 +75,14 @@ from parax.converters import (
     as_param as as_param,
 )
 
-from parax import optimize as optimize
+
+from parax import (
+    constant as constant,
+    tagged as tagged,
+    bounded as bounded,
+    unwrappables as unwrappables,
+    variables as variables,
+    constraints as constraints,
+    filters as filters,
+    converters as converters,
+)
